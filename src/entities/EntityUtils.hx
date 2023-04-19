ff9f048b5d611f8d13db716d9efab98443f48391
Ian Harrigan <ianharrigan@hotmail.com>
2023-04-19 18:24:46 +0200

    date could be null

--- src/entities/EntityUtils.hx
+++ src/entities/EntityUtils.hx
@@ -4,10 +4,16 @@ using StringTools;
 
 class EntityUtils {
     public static function dateToIso8601(date:Date):String {
+        if (date == null) {
+            return null;
+        }
         return DateTools.format(date, "%Y-%m-%dT%H:%M:%SZ");
     }
 
     public static function iso8601ToDate(dateString:String):Date {
+        if (dateString == null) {
+            return null;
+        }
         dateString = dateString.replace("Z", "");
         dateString = dateString.replace("T", " ");
         return Date.fromString(dateString);
